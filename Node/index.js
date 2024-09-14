17d298c9270b88204d69cd1cc876e47ceb70dda8
Karolina Queiroz <queirozkarolina30@gmail.com>
2024-09-14 18:45:58 -0300

    Inserindo o exercício 'NODE1' da disciplina Node

--- Node/index.js
+++ Node/index.js
@@ -1,21 +1,30 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer(function(red,res){
-    res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"})
-    res.write("Instituto de Computação");
-    res.end();
+const server = http.createServer(function(req,res){
+    const directoryPath = path.join('C:', 'Users', 'queir', 'OneDrive', 'Documentos');
 
-    //Renomear o arquivo
-    fs.rename('imagem1.png', 'imagem2.png', function(err){
+    //Função readdir
+    fs.readdir(directoryPath, (err, files) =>{
         if(err){
-            console.log('Erro ao renomear o arquivo', err);
-        }else{
-            console.log('Arquivo renomeado com sucesso.');
+            res.writeHead(500, {"Content-Type":"text/html;charset=utf-8"});
+            res.write("Erro ao ler o diretório.");
+            return res.end();
         }
+
+        // Cria a lista de arquivos em HTML
+        let fileList = '<ul>';
+        files.forEach(file => {
+            fileList += `<li>${file}</li>`;
+        });
+        fileList += '</ul>';
+
+        // Retorna a resposta com a lista de arquivos
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(`<h1>Arquivos no diretorio:</h1>${fileList}`);
+        return res.end();
     });
 });
-
-
 
 server.listen(3333);
