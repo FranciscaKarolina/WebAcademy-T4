5545d99ca68602f58dd5b579422bde90d31145c0
Karolina Queiroz <queirozkarolina30@gmail.com>
2024-09-14 18:00:46 -0300

    Usando o método rename do módulo fs para renomear arquivos

--- Node/index.js
+++ Node/index.js
@@ -1,9 +1,21 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer(function(red,res){
     res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"})
     res.write("Instituto de Computação");
     res.end();
+
+    //Renomear o arquivo
+    fs.rename('imagem1.png', 'imagem2.png', function(err){
+        if(err){
+            console.log('Erro ao renomear o arquivo', err);
+        }else{
+            console.log('Arquivo renomeado com sucesso.');
+        }
+    });
 });
+
+
 
 server.listen(3333);
