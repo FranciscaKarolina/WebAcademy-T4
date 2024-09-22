3a8516a6e25a23aeed157442ed5988b9b8e11bf5
Karolina Queiroz <queirozkarolina30@gmail.com>
2024-09-22 16:43:27 -0300

    Finalizando a atualização do exercício 1

--- Node/src/index.js
+++ Node/src/index.js
@@ -2,35 +2,51 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require("dotenv") 
+const {createLink} = require('./util');
 
 //carregar o arquivo .env
 dotenv.config({path: `.env.${process.env.NODE_ENV}`});
 
 const PORT = process.env.PORT ?? 7878;
 
-const server = http.createServer(function(req,res){
-    const directoryPath = path.join('C:', 'Users', 'queir', 'OneDrive', 'Documentos');
+const directoryPath = path.join('C:', 'Users', 'queir', 'OneDrive', 'Documentos');
 
-    //Função readdir
-    fs.readdir(directoryPath, (err, files) =>{
-        if(err){
-            res.writeHead(500, {"Content-Type":"text/html;charset=utf-8"});
-            res.write("Erro ao ler o diretório.");
+const server = http.createServer((req,res) => {
+    if(req.url === '/'){
+        //Função readdir
+        fs.readdir(directoryPath, (err, files) =>{
+            if(err){
+                res.writeHead(500, {"Content-Type":"text/html;charset=utf-8"});
+                res.write("Erro ao ler o diretório.");
+                return res.end();
+            }
+    
+            // Cria a lista de links
+            let fileList = '';
+            files.forEach(file => {
+                fileList += createLink(file);
+            });
+    
+            // Retorna a resposta 
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(`<h1>Arquivos no diretorio:</h1>${fileList}`);
             return res.end();
-        }
-
-        // Cria a lista de arquivos em HTML
-        let fileList = '<ul>';
-        files.forEach(file => {
-            fileList += `<li>${file}</li>`;
         });
-        fileList += '</ul>';
+    }else{
+        // Exibir conteúdo do arquivo
+        const filePath = path.join(directoryPath, req.url);
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/html;charset=utf-8' });
+                res.write("Arquivo não encontrado.");
+                return res.end();
+            }
 
-        // Retorna a resposta com a lista de arquivos
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(`<h1>Arquivos no diretorio:</h1>${fileList}`);
-        return res.end();
-    });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(`<pre>${data}</pre><br><a href="/">Voltar</a>`);
+            return res.end();
+        });   
+    }
 });
 
 server.listen(PORT, () => {
